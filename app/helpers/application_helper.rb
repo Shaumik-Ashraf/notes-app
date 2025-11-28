84ab7679277fb70adde5ab6c77c71980a66595be
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-11-28 18:31:38 -0500

    add markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, strikethrough: true, superscript: true)
+
+  def markdown(s)
+    sanitize MARKDOWN.render(s)
+  end
 end
