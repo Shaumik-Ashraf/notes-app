41a2f22ecfff26d419b4d6e83c19e1a7f07312c9
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-11-30 11:25:35 -0500

    improve notes#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,24 @@
 module ApplicationHelper
   MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, strikethrough: true, superscript: true)
-
+  TIME_FORMAT = "%r %x"
+  
+  # @param s [String]
   def markdown(s)
     sanitize MARKDOWN.render(s)
+  end
+
+  # @param d [DateTime, #to_datetime, nil]
+  # @return [String]
+  # @raises [StandardError]
+  def time_format(d)
+    if d.nil?
+      "N/A"
+    elsif d.is_a? DateTime
+      d.strftime(TIME_FORMAT)
+    elsif d.respond_to? :to_datetime
+      time_format(d.to_datetime)
+    else
+      raise StandardError, "Cannot convert #{d} into time format"
+    end
   end
 end
