42eeffb1150d7832084076c08ef520db8c72815e
Shaumik-Ashraf <a.shaumik@gmail.com>
2025-12-01 13:31:05 -0500

    stricter whitelist for markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,28 @@
 module ApplicationHelper
-  MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, strikethrough: true, superscript: true)
+  MARKDOWN = Redcarpet::Markdown.new(
+    Redcarpet::Render::HTML.new(
+      filter_html: true,
+      no_images: false,
+      no_links: false,
+      safe_links_only: true,
+      hard_wrap: true
+    ),
+    autolink: true,
+    tables: true,
+    strikethrough: true,
+    superscript: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true
+  )
   TIME_FORMAT = "%r %x"
 
   # @param s [String]
   def markdown(s)
-    sanitize MARKDOWN.render(s)
+    sanitize(
+      MARKDOWN.render(s),
+      tags: %w[p br strong em ul ol li h1 h2 h3 h4 h5 h6 code pre blockquote table thead tbody tr th td a hr],
+      attributes: { "a" => [ "href" ] }
+    )
   end
 
   # @param d [DateTime, #to_datetime, nil]
